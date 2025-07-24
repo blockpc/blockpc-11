677b3bdb47e241403713590e0d1aeea3b3cc22a2
blockpc <soporte.blockpc@gmail.com>
2025-07-24 11:03:49 -0400

    mejoras recomendadas

--- resources/views/components.blade.php
+++ resources/views/components.blade.php
@@ -2,7 +2,7 @@
 <div class="flex text-xs font-semibold">
     <div class="inline-flex relative items-center cursor-pointer">
         <input type="checkbox" class="sr-only peer" />
-        <div class="w-10 h-5 bg-gray-700 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top after:left-0 after:bg-white after:border-blue-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-blue-600 peer-checked:bg-blue-600"></div>
+        <div class="w-10 h-5 bg-gray-700 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0 after:left-0 after:bg-white after:border-blue-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-blue-600 peer-checked:bg-blue-600"></div>
         <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300 whitespace-nowrap">Si</span>
         <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300 whitespace-nowrap">No</span>
     </label>
@@ -12,7 +12,7 @@
 <div class="flex text-xs font-semibold">
     <div class="inline-flex relative items-center cursor-pointer">
         <input type="checkbox" class="sr-only peer" />
-        <div class="w-10 h-5 bg-blue-700 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-red-300 dark:peer-focus:ring-red-800 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top after:left-0 after:bg-white after:border-red300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-red-600 peer-checked:bg-red-600"></div>
+        <div class="w-10 h-5 bg-blue-700 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-red-300 dark:peer-focus:ring-red-800 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0 after:left-0 after:bg-white after:border-red300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-red-600 peer-checked:bg-red-600"></div>
         <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300 whitespace-nowrap">Si</span>
         <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300 whitespace-nowrap">No</span>
     </label>
@@ -22,7 +22,7 @@
 <div class="flex text-xs font-semibold">
     <div class="inline-flex relative items-center cursor-pointer">
         <input type="checkbox" class="sr-only peer" />
-        <div class="w-10 h-5 bg-red-700 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top after:left-0 after:bg-white after:border-blue-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-blue-600 peer-checked:bg-blue-600"></div>
+        <div class="w-10 h-5 bg-red-700 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0 after:left-0 after:bg-white after:border-blue-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-blue-600 peer-checked:bg-blue-600"></div>
         <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300 whitespace-nowrap">Si</span>
         <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300 whitespace-nowrap">No</span>
     </label>
@@ -32,7 +32,7 @@
 <div class="flex text-xs font-semibold">
     <div class="inline-flex relative items-center cursor-pointer">
         <input type="checkbox" class="sr-only peer" />
-        <div class="w-10 h-5 bg-red-700 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top after:left-0 after:bg-red-600 after:border-green-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-green-600 peer-checked:bg-green-600"></div>
+        <div class="w-10 h-5 bg-red-700 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0 after:left-0 after:bg-red-600 after:border-green-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-green-600 peer-checked:bg-green-600"></div>
         <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300 whitespace-nowrap">Si</span>
         <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300 whitespace-nowrap">No</span>
     </label>
--- resources/views/components/toggle-sm.blade.php
+++ resources/views/components/toggle-sm.blade.php
@@ -10,7 +10,7 @@
 <div {{ $attributes->except(['wire:model.live'])->merge(['class' => 'flex text-xs font-semibold']) }} x-data="{toogle: @entangle($attributes->wire('model')->value()).live }">
     <label for="{{$name}}" class="inline-flex relative items-center cursor-pointer">
         <input type="checkbox" name="{{$name}}" id="{{$name}}" class="sr-only peer" {{ $attributes->except('class') }} {{$disabled ? 'disabled' : ''}} />
-        <div class="w-10 h-5 bg-{{$default}}-700 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-{{$color}}-300 dark:peer-focus:ring-{{$color}}-800 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top after:left-0 after:bg-white after:border-{{$color}}-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-{{$color}}-600 peer-checked:bg-{{$color}}-600"></div>
+        <div class="w-10 h-5 bg-{{$default}}-700 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-{{$color}}-300 dark:peer-focus:ring-{{$color}}-800 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0 after:left-0 after:bg-white after:border-{{$color}}-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-{{$color}}-600 peer-checked:bg-{{$color}}-600"></div>
         <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300 whitespace-nowrap" x-show="toogle" x-cloak>{{__($yes)}}</span>
         <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300 whitespace-nowrap" x-show="!toogle" x-cloak>{{__($not)}}</span>
     </label>
