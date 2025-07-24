1c46aa6ec5b05375b3316c0745981320ad287183
blockpc <soporte.blockpc@gmail.com>
2025-07-24 16:54:51 -0400

    fix PermissionList and some blade

--- Blockpc/App/Console/Classes/PermissionList.php
+++ Blockpc/App/Console/Classes/PermissionList.php
@@ -14,24 +14,52 @@ final class PermissionList
      * - description: Descripción del permiso
      * - display_name: Nombre para mostrar del permiso
      * - guard_name: Nombre del guard (opcional, por defecto 'web')
-     * [name, key, description, display_name, guard_name (opcional:web)]
+     * [name, key, description, display_name, guard_name (, opcional:web)]
      */
     public static function all(): array
+    {
+        return [
+            ...self::system(),
+            ...self::users(),
+            ...self::roles(),
+            ...self::permissions(),
+        ];
+    }
+
+    private static function system(): array
     {
         return [
             ['super admin', 'sudo', 'Permiso de Super Usuario. El usuario con este permiso tiene acceso total al sistema. No necesita ningún otro permiso', 'Super usuario'],
             ['jobs control', 'jobs', 'Controla las tareas pendientes y fallidas del sistema', 'Control Tareas'],
             ['settings control', 'settings', 'Controla el acceso a la configuración del sistema', 'Control Configuración'],
+        ];
+    }
+
+    private static function users(): array
+    {
+        return [
             ['user list', 'users', 'Permite acceder al listado de usuarios', 'Listado de Usuarios'],
             ['user create', 'users', 'Permite crear un nuevo usuario', 'Crear Usuarios'],
             ['user update', 'users', 'Permite actualizar la información de un usuario', 'Actualizar Usuarios'],
             ['user delete', 'users', 'Permite eliminar un usuario', 'Eliminar Usuarios'],
             ['user restore', 'users', 'Permite restaurar un usuario eliminado anteriormente', 'Restaurar Usuario'],
+        ];
+    }
+
+    private static function roles(): array
+    {
+        return [
             ['role list', 'roles', 'Permite acceder al listado de cargos', 'Listado de Cargos'],
             ['role create', 'roles', 'Permite crear un cargo', 'Crear Cargos'],
             ['role update', 'roles', 'Permite actualizar un cargo', 'Actualizar Cargos'],
             ['role delete', 'roles', 'Permite eliminar un cargo', 'Eliminar Cargos'],
             ['role restore', 'roles', 'Permite restaurar un cargo eliminado anteriormente', 'Restaurar Cargo'],
+        ];
+    }
+
+    private static function permissions(): array
+    {
+        return [
             ['permission list', 'permissions', 'Permite el acceso a la lista de permisos', 'Listado de Permisos'],
             ['permission update', 'permissions', 'Permite actualizar la información de un permiso', 'Actualizar Permisos'],
         ];
--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -8,62 +8,91 @@
     </x-page-header>
 
     <section class="mt-2 mx-auto w-full">
-        <div class="grid gap-4">
+        <div class="grid gap-8">
 
-            <x-fieldset title="Sobre el Perfil">
-                <form class="w-full md:w-1/2" wire:submit.prevent="updateProfile">
-                    <div class="grid grid-cols-2 gap-4">
-                        <x-inputs.text name="firstname" label="{{ __('pages.profile.form.firstname') }}" wire:model="firstname" />
-
-                        <x-inputs.text name="lastname" label="{{ __('pages.profile.form.lastname') }}" wire:model="lastname" />
-
-                        <div class="col-span-2">
-                            <x-inputs.text name="email" label="{{ __('pages.profile.form.email') }}" wire:model="email" autocomplete="username" />
-                        </div>
-
-                        <div class="col-span-2">
-                            <x-photo-user name="profile_user" :photo="$photo" wire:model="photo" />
-                        </div>
-
-                        <div class="col-span-2 flex justify-end">
-                            <x-buttons.submit class="btn-primary">
-                                <span>{{ __('pages.profile.buttons.update') }}</span>
-                            </x-buttons.submit>
-                        </div>
+            <x-fieldset>
+                <div class="grid lg:grid-cols-3 gap-4">
+                    <div class="flex flex-col">
+                        <legend class="upper-legend">{{__('Sobre el Perfil')}}</legend>
+                        <p class="legend italic">{{__('Datos asociados al usuario')}}</p>
                     </div>
-                </form>
+
+                    <div class="lg:col-span-2">
+                        <form wire:submit.prevent="updateProfile">
+                            <div class="grid grid-cols-2 gap-4">
+                                <x-inputs.text name="firstname" label="{{ __('pages.profile.form.firstname') }}" wire:model="firstname" />
+
+                                <x-inputs.text name="lastname" label="{{ __('pages.profile.form.lastname') }}" wire:model="lastname" />
+
+                                <div class="col-span-2">
+                                    <x-inputs.text name="email" label="{{ __('pages.profile.form.email') }}" wire:model="email" autocomplete="username" />
+                                </div>
+
+                                <div class="col-span-2">
+                                    <x-photo-user name="profile_user" :photo="$photo" wire:model="photo" />
+                                </div>
+
+                                <div class="col-span-2 flex justify-end">
+                                    <x-buttons.submit class="btn-primary">
+                                        <span>{{ __('pages.profile.buttons.update') }}</span>
+                                    </x-buttons.submit>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </x-fieldset>
 
-            <x-fieldset title="Sobre la Contraseña">
-                <form class="w-full md:w-1/2" wire:submit.prevent="updatePassword">
-                    <div class="grid gap-4">
-                        <x-inputs.password name="current_password" label="{{ __('pages.profile.form.current_password') }}" wire:model="current_password" autocomplete="new-password" />
-
-                        <x-inputs.password name="password" label="{{ __('pages.profile.form.password') }}" wire:model="password" autocomplete="new-password" />
-
-                        <x-inputs.password name="password_confirmation" label="{{ __('pages.profile.form.password_confirmation') }}" wire:model="password_confirmation" autocomplete="new-password" />
+            <x-fieldset>
+                <div class="grid lg:grid-cols-3 gap-4">
+                    <div class="flex flex-col">
+                        <legend class="upper-legend">{{__('Sobre la Contraseña')}}</legend>
+                        <p class="legend italic">{{__('Cambio / actualización de clave de usuario.')}}</p>
                     </div>
-                    <div class="flex justify-end mt-4">
-                        <x-buttons.submit class="btn-primary">
-                            <span>{{ __('pages.profile.buttons.update-password') }}</span>
-                        </x-buttons.submit>
+
+                    <div class="lg:col-span-2">
+                        <form wire:submit.prevent="updatePassword">
+                            <div class="grid gap-4">
+                                <x-inputs.password name="current_password" label="{{ __('pages.profile.form.current_password') }}" wire:model="current_password" autocomplete="new-password" />
+
+                                <x-inputs.password name="password" label="{{ __('pages.profile.form.password') }}" wire:model="password" autocomplete="new-password" />
+
+                                <x-inputs.password name="password_confirmation" label="{{ __('pages.profile.form.password_confirmation') }}" wire:model="password_confirmation" autocomplete="new-password" />
+
+                                <div class="flex justify-end">
+                                    <x-buttons.submit class="btn-primary">
+                                        <span>{{ __('pages.profile.buttons.update-password') }}</span>
+                                    </x-buttons.submit>
+                                </div>
+                            </div>
+                        </form>
                     </div>
-                </form>
+                </div>
             </x-fieldset>
 
-            <x-fieldset title="Eliminación de la Cuenta">
-                <form class="w-full md:w-1/2" wire:submit.prevent="deleteAccount">
-                    <div class="grid gap-4">
-                        <x-inputs.text type="email" name="delete_email" label="{{ __('pages.profile.form.email') }}" wire:model="delete_email" />
+            <x-fieldset>
+                <div class="grid lg:grid-cols-3 gap-4">
+                    <div class="flex flex-col">
+                        <legend class="upper-legend">{{__('Eliminación de la Cuenta')}}</legend>
+                        <p class="legend italic">{{__('Eliminación de la cuenta de usuario.')}}</p>
+                    </div>
 
-                        <x-inputs.password name="delete_current_password" label="{{ __('pages.profile.form.current_password') }}" wire:model="delete_current_password" autocomplete="new-password" />
+                    <div class="lg:col-span-2">
+                        <form wire:submit.prevent="deleteAccount">
+                            <div class="grid gap-4">
+                                <x-inputs.text type="email" name="delete_email" label="{{ __('pages.profile.form.email') }}" wire:model="delete_email" />
+
+                                <x-inputs.password name="delete_current_password" label="{{ __('pages.profile.form.current_password') }}" wire:model="delete_current_password" autocomplete="new-password" />
+
+                                <div class="flex justify-end">
+                                    <x-buttons.submit class="btn-danger">
+                                        <span>{{ __('pages.profile.buttons.delete-account') }}</span>
+                                    </x-buttons.submit>
+                                </div>
+                            </div>
+                        </form>
                     </div>
-                    <div class="flex justify-end mt-4">
-                        <x-buttons.submit class="btn-danger">
-                            <span>{{ __('pages.profile.buttons.delete-account') }}</span>
-                        </x-buttons.submit>
-                    </div>
-                </form>
+                </div>
             </x-fieldset>
 
         </div>
